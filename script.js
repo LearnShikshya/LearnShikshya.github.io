2fc4349dc21fdfc587a657ff3f65ddc88cb23326
LearnShikshya <learnshikshya@gmail.com>
2023-02-18 10:17:27 +0545

    Made some changes in the index file. Some New Content.

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function w3_open() {
       document.getElementById("mySidebar").style.display = "none"
     }
   }
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) =>{
+    console.log(entry)
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach( (el) => observer.observe(el) );
